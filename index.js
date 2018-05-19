1b877831523200faab0bff5a1fdcb146d6710b2a
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-19 21:11:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.getElementsByClassName('ranked-list');
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = (i + n).toString();
+  }
+}
+
+function deepestChild() {
+  document.getElementById('grand-node')
+}
