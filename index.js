cdc31a6df1d0132f040677ae1b4474d3be4fc0de
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-19 21:52:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   
-  var grandNode = document.getElementById('grand-node').querySelector('div')[3];
-  return grandNode;
+  var grandNode = document.getElementById('grand-node');
   
   
-  //for (let i = 0; i < grandNode.length; i++) {
-    //var solution = grandNode.querySelector('div[i]').innerHTML;
-    //return solution;
+  for (let i = 0; i < grandNode.length; i++) {
+    var solution = grandNode.querySelector('div[i]').innerHTML;
+    return solution;
   
 }
